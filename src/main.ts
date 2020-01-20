c287c9ca345da4f0fda9fbedb3f0af72bc9163fb
Fernando Silva <fdograph@gmail.com>
2020-01-20 20:10:57 +0100

    First Commit

---
+++ src/main.ts
@@ -0,0 +1,70 @@
+const rutLikePattern = (): RegExp => /^(\d{0,2})\.?(\d{3})\.?(\d{3})-?(\d|k)$/gi;
+const suspiciousRutPattern = (): RegExp => /^(\d)\1?\.?(\1{3})\.?(\1{3})-?(\d|k)?$/gi;
+
+export const isRutLike = (rut: string): boolean => rutLikePattern().test(rut);
+export const isSuspiciousRut = (rut: string): boolean => suspiciousRutPattern().test(rut);
+export const cleanRut = (rut: string): string => isRutLike(rut) ? rut.replace(/[^0-9k]/gi, '') : '';
+export const getRutDigits = (rut: string): string => cleanRut(rut).slice(0, -1);
+export const getRutVerifier = (rut: string): string => cleanRut(rut).slice(-1);
+
+export enum RutFormat {
+  DOTS,
+  DASH,
+  DOTS_DASH,
+};
+
+export const formatRut = (rut: string, format = RutFormat.DASH): string => {
+  if (!isRutLike(rut)) return rut;
+
+  switch (format) {
+    case RutFormat.DOTS:
+      return rut.replace(
+        rutLikePattern(),
+        (...args) => `${args[1] ? `${args[1]}.` : ''}${args[2]}.${args[3]}${args[4]}`,
+      );
+
+    case RutFormat.DASH:
+      return rut.replace(rutLikePattern(), '$1$2$3-$4');
+
+    case RutFormat.DOTS_DASH:
+      return rut.replace(
+        rutLikePattern(),
+        (...args) => `${args[1] ? `${args[1]}.` : ''}${args[2]}.${args[3]}-${args[4]}`,
+      );
+
+    default:
+      return rut.replace(rutLikePattern(), '$1$2$3$4');
+  }
+};
+
+export const calculateRutVerifier = (digits: string): string => {
+  let sum = 0;
+  let mul = 2;
+
+  let i = digits.length;
+  while (i--) {
+    sum = sum + parseInt(digits.charAt(i)) * mul;
+    if (mul % 7 === 0) { mul = 2; }
+    else { mul++; }
+  }
+
+  const res = sum % 11;
+
+  if (res === 0) { return '0'; }
+  else if (res === 1) { return 'k'; }
+
+  return `${(11 - res)}`;
+};
+
+export const validateRut = (rut: string, noSuspicious = true): boolean => {
+  if (!isRutLike(rut)) return false;
+  if (noSuspicious && isSuspiciousRut(rut)) return false;
+  if (getRutVerifier(rut) !== calculateRutVerifier(getRutDigits(rut))) return false;
+  return true;
+};
+
+export const generateRut = (): string => {
+  const digits = Math.floor(10000003 + Math.random() * 90000000).toString();
+  const verifier = calculateRutVerifier(digits);
+  return formatRut(digits + verifier);
+};
---
+++ src/tests/index.test.ts
@@ -0,0 +1,172 @@
+import {
+  isRutLike,
+  isSuspiciousRut,
+  cleanRut,
+  getRutDigits,
+  getRutVerifier,
+  calculateRutVerifier,
+  validateRut,
+  formatRut,
+  generateRut,
+  RutFormat
+} from '../main';
+
+describe('isRutLike', () => {
+  it('Should validate Regex pattern for a rut-like string', () => {
+    const validCases: string[] = [
+      '9.999.999-9',
+      '14355245-5',
+      '34566754-k',
+      '12.344.568-4',
+      '32.456.356-k',
+      '543.567-6',
+    ];
+    const invalidCases: string[] = [
+      '23.432.432-t',
+      'dfsg24rfr2f3-',
+      '13.354322-g',
+      '13424-2',
+    ];
+
+    validCases.forEach(rut => {
+      expect(isRutLike(rut)).toEqual(true);
+    });
+
+    invalidCases.forEach(rut => {
+      expect(isRutLike(rut)).toEqual(false);
+    });
+  });
+});
+
+describe('isSuspiciousRut', () => {
+  it('Should validate Regex pattern for a suspicious-rut string', () => {
+    const validCases = [
+      '11.111.111-1',
+      '2.222.222-k',
+      '22222222',
+    ];
+    const invalidCases = [
+      '18585543-0',
+      '2s222t222-k',
+      '23.432.432-t',
+      'dfsg24rfr2f3-',
+      '13.354322-g',
+      '13424-2',
+    ];
+    
+    validCases.forEach(rut => {
+      expect(isSuspiciousRut(rut)).toEqual(true);
+    });
+
+    invalidCases.forEach(rut => {
+      expect(isSuspiciousRut(rut)).toEqual(false);
+    });
+  });
+});
+
+describe('cleanRut', () => {
+  it('Should clean a rut-like string or return an empty string', () => {
+    expect(cleanRut('13.543.343-k')).toEqual('13543343k');
+    expect(cleanRut('22456765-3')).toEqual('224567653');
+    expect(cleanRut('11111111-1')).toEqual('111111111');
+    expect(cleanRut('fnekcsnsdk')).toEqual('');
+    expect(cleanRut('')).toEqual('');
+    expect(cleanRut('35fdlp34/d')).toEqual('');
+  });
+});
+
+describe('getRutDigits', () => {
+  it('Should get the digits of rut-like string or an empty string', () => {
+    expect(getRutDigits('13.543.343-k')).toEqual('13543343');
+    expect(getRutDigits('fnekcsnsdk')).toEqual('');
+  });
+});
+
+describe('getRutVerifier', () => {
+  it('Should get the verifier of rut-like string or an empty string', () => {
+    expect(getRutVerifier('13.543.343-k')).toEqual('k');
+    expect(getRutVerifier('fnekcsnsdk')).toEqual('');
+  });
+});
+
+describe('calculateRutVerifier', () => {
+  it('Should calculate the verifier for provided digits', () => {
+    const tests = [
+      ['7775735', 'k'],
+      ['18585543', '0'],
+      ['18348353', '6'],
+    ];
+
+    tests.forEach(([digits, expectedVerifier]) => {
+      expect(calculateRutVerifier(digits)).toEqual(expectedVerifier);
+    });
+  });
+})
+
+describe('validateRut', () => {
+  it('Should validate a rut-like string', () => {
+    const validRuts = ['7775735-k', '18585543-0', '18348353-6'];
+    const invalidRuts = ['', '9.999.999-9', '14355245-5', '34566754-k', '12.344.568-4', '32.456.356-k'];
+
+    validRuts.forEach(test => {
+      expect(validateRut(test)).toEqual(true);
+    });
+
+    invalidRuts.forEach(test => {
+      expect(validateRut(test)).toEqual(false);
+    });
+  });
+
+  it('Should skip suspicious-rut validation when toggling noSuspicious param', () => {
+    const suspiciousValidRuts = ['11111111-1', '22222222-2', '99999999-9'];
+    const suspiciousInvalidRuts = ['3333333-3', '2222222-k', ''];
+
+    suspiciousValidRuts.forEach(rut => {
+      const on = validateRut(rut);
+      const off = validateRut(rut, false);
+
+      expect(on).toEqual(false);
+      expect(off).toEqual(true);
+    });
+
+    suspiciousInvalidRuts.forEach(rut => {
+      const on = validateRut(rut, true);
+      const off = validateRut(rut, false);
+
+      expect(on).toEqual(false);
+      expect(off).toEqual(false);
+    });
+  });
+});
+
+describe('formatRut', () => {
+  it('Should format a rut-like string or return it intact', () => {
+    const longrutLikeStr = '44.333.222-1';
+    const shortutLikeStr = '333.222-1';
+
+    expect(formatRut(longrutLikeStr)).toEqual('44333222-1');
+    expect(formatRut(longrutLikeStr, RutFormat.DASH)).toEqual('44333222-1');
+    expect(formatRut(longrutLikeStr, RutFormat.DOTS_DASH)).toEqual('44.333.222-1');
+    expect(formatRut(longrutLikeStr, RutFormat.DOTS)).toEqual('44.333.2221');
+    expect(formatRut(longrutLikeStr, null)).toEqual('443332221');
+
+    expect(formatRut(shortutLikeStr)).toEqual('333222-1');
+    expect(formatRut(shortutLikeStr, RutFormat.DASH)).toEqual('333222-1');
+    expect(formatRut(shortutLikeStr, RutFormat.DOTS_DASH)).toEqual('333.222-1');
+    expect(formatRut(shortutLikeStr, RutFormat.DOTS)).toEqual('333.2221');
+    expect(formatRut(shortutLikeStr, null)).toEqual('3332221');
+
+    expect(formatRut('ne93jkdf39f-')).toEqual('ne93jkdf39f-');
+    expect(formatRut('')).toEqual('');
+  })
+});
+
+describe('generateRut', () => {
+  it('Should generate valid Ruts', () => {
+    let runs = 100;
+
+    while (runs--) {
+      expect(validateRut(generateRut())).toEqual(true);
+    }
+  });
+});
